7fa72b156e7d1a21be97ba9eee1982f7bf22f1a2
wagneryhr-eng <wagner.yhr@gmail.com>
2026-03-28 03:56:49 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,24 @@
-const CACHE_NAME = 'proseka-tool-v1';
+const CACHE_NAME = 'proseka-tool-v2';
 const ASSETS = [
-  'index.html',
-  'manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
   );
 });
 
